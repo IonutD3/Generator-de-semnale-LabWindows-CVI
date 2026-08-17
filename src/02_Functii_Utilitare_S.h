c98398bf1fc0a08a5e0e761410be704d4b6825e1
IonutD3 <146918885+IonutD3@users.noreply.github.com>
2026-08-17 15:17:23 +0300

    Add files via upload

---
+++ src/02_Functii_Utilitare_S.h
@@ -0,0 +1,29 @@
+/* Functii utilitare pentru stergerea graficului si oprirea aplicatiei. */
+
+int CVICALLBACK stergere_grafice(int panel, int control, int event,
+                                 void *callbackData, int eventData1, int eventData2)
+{
+    switch (event)
+    {
+        case EVENT_COMMIT:
+            DeleteGraphPlot(panelHandle, PANEL_GRAPH, -1, VAL_IMMEDIATE_DRAW);
+            tk_1 = 0;
+            tk = 0;
+            break;
+    }
+
+    return 0;
+}
+
+int CVICALLBACK QuitCallback(int panel, int control, int event,
+                             void *callbackData, int eventData1, int eventData2)
+{
+    switch (event)
+    {
+        case EVENT_COMMIT:
+            QuitUserInterface(0);
+            break;
+    }
+
+    return 0;
+}
